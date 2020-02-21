d0087eedf47d961e5b6ff3f4e7191a376d05524a
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-02-21 13:39:56 -0500

    add scrape duration

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 import logging
+from datetime import datetime
+import time
 from definitions import DIR_DATA
 from pdf_downloader.helper.misc import delete_dir_contents
 from pdf_downloader.scrape.get_pdf import get_pdf
@@ -9,6 +11,7 @@ from logs.config.logging import logs_config
 def main():
 
     # init logging
+    scrape_start_time = datetime.now()
     logs_config()
     logging.info('Beginning scrape')
 
@@ -16,7 +19,7 @@ def main():
     list_of_filing_ids = [
         "336344",  # samuel doctor
         "331887",  # phil heasley
-        "332791"
+        "332791"  # Andre Del Valle
     ]
     filing_id = "336344"
 
@@ -32,6 +35,12 @@ def main():
         get_pdf(filing_id)
 
     # complete
+    scrape_end_time = datetime.now()
+    # scrape_duration = int(int(time.mktime(scrape_end_time.timetuple()) -
+    #                           time.mktime(scrape_start_time.timetuple()))/ 60)
+    scrape_duration = (scrape_end_time - scrape_start_time).total_seconds()
+    logging.info(f'Total scrape time: {scrape_duration /60} minutes')
+    logging.info(f'Time per item scraped: {round(scrape_duration / len(list_of_filing_ids), 2)} sec')
     logging.info("Scrape complete")
 
 
