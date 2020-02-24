cb1bf9d79d66128b55438833b141e1bb32c84fd9
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-02-24 13:47:21 -0500

    clean up comments

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -1,8 +1,5 @@
-
-from pathlib import Path
 import logging
 from datetime import datetime
-import time
 from definitions import DIR_DATA
 from pdf_downloader.helper.misc import delete_dir_contents
 from pdf_downloader.scrape.get_pdf import get_pdf
@@ -15,7 +12,7 @@ def main():
     logs_config()
     logging.info('Beginning scrape')
 
-    # filing ID
+    # list of target filing IDs
     list_of_filing_ids = [
         "336344",  # samuel doctor
         "331887",  # phil heasley
@@ -36,8 +33,6 @@ def main():
 
     # complete
     scrape_end_time = datetime.now()
-    # scrape_duration = int(int(time.mktime(scrape_end_time.timetuple()) -
-    #                           time.mktime(scrape_start_time.timetuple()))/ 60)
     scrape_duration = (scrape_end_time - scrape_start_time).total_seconds()
     logging.info(f'Total scrape time: {scrape_duration /60} minutes')
     logging.info(f'Time per item scraped: {round(scrape_duration / len(list_of_filing_ids), 2)} sec')
