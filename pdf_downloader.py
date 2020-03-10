c11e33b4c0d0a2d680a2af6afe4a90cb8c4a056e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-10 17:16:52 -0400

    add sleep between loop iterations

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -4,6 +4,7 @@ from definitions import DIR_DATA
 from pdf_downloader.helper.misc import delete_dir_contents
 from pdf_downloader.scrape.get_pdf import get_pdf
 from logs.config.logging import logs_config
+import time
 
 def main():
 
@@ -12,6 +13,7 @@ def main():
     logs_config()
     logging.info('Beginning scrape')
 
+    # SET VARS
     # list of target filing IDs
     list_of_filing_ids = [
         "336344",  # samuel doctor
@@ -19,6 +21,7 @@ def main():
         "332791",  # Andre Del Valle
         "209028"  # thomas murt
     ]
+    sleep_time = 5
 
     # create or clean up PDF download dir
     if DIR_DATA.is_dir():
@@ -30,6 +33,10 @@ def main():
     # get pdf
     for filing_id in list_of_filing_ids:
         get_pdf(filing_id)
+        # sleep in order to avoid overloading Ethics Commission website
+        logging.info(f'Sleeping for {sleep_time} seconds...')
+        time.sleep(sleep_time)
+        logging.info('...Finished sleeping!')
 
     # complete
     scrape_end_time = datetime.now()
